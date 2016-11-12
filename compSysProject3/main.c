6cdd9b47af4b3ecf39f9646003bde610048893af
mdonnelly141 <matthewdonne@umass.edu>
2016-11-11 22:34:28 -0500

    Update main.c
    -confirm array/pointer initiations
    -turn k equations into an array?
    To-Do:
          Finish file reading, create test .txts......Code is hardcoded for 4 cores to begin with
          Finish implementing RK and related functions
          Implement age (should operate in parallel to temperature, almost a copy paste of RK temperature algorithm)

--- compSysProject3/main.c
+++ compSysProject3/main.c
@@ -11,54 +11,91 @@
 #define EA = 0.8				//Activation Energy
 #define h = 0.005				//step size for RK Algorithm
 const char EOL = '\n';			//end of line character
+double ambient = 300;			//default ambient temperature of 300k
 
 
-//Runge-Kutta Algorithm
-double rk(double n){	
-	for(int a=0; a<n; a++){
-		//for loop for iterating thru rk calculations
-	}
+//////////////////////Runge-Kutta Algorithm//////////////////////////////////
+double *rk(double numCores){   //returns pointer	
+	for(int i=0; i<numCores; i++)
+		k1 = h*f(i, res[row][col], cap[coreCount], pow[coreCount]);  
+
+
+	
     	
 }
+/////////////////////////////////////////////////////////////////////////////
 
-//function utilized within RK 
-double f(double R, double C, double P){
-	//w(t) equation? or k equation. also how many 
-
+//////////////////////Functions utilized within RK/////////////////////////// 
+double f(int core, res[row][col], cap[coreCount], pow[coreCount]){
+	k = (P - sum())/R;
 	return k;
 }
 
+double sum(double core, double relativeCore){
+	double ans = 0;
+	for(int si = 0; si < core; si++){
+		ans = temp[si] - ambient;
+	}
+	return ans;
+}
+////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, const char * argv[]) {
-	int i = 0;			//column counter
-	int j = 0;			//row counter
-	int t = 0;
+	int coreCount = 0;	//counter for current core
+	int col = 0;		//column counter
+	int row = 0;		//row counter
+	char c;				//holds characters for file reading
 	int numCores = 0;	//number of cores
 
 
-	//File Reading
 	FILE *paramFile = fopen(argv[1], "r"); // reads parameter File
     assert(paramFile != NULL); //check that file exists
+    ///////////////////////GENERAL CASE FOR N CORES
+    /*while(c = getc(paramFile) && !(c = EOL)){ //checks that there are characters and that end of line hasn't been reached
+    	if(c = ' ') 
+    		numCores++; //count core
+    }
+    numCores+=1; //count last core, holds total number of cores*/   
+    numCores = 4; //hardcoded 4 core example
 
+    //////////////ARRAY ESTABLISHMENT/////////////////////////////
 
-    /*
-    while ((c = getc(paramFile)) != EOF && kLength < 256){ // uses only first 256 bytes of keyFile
-        key[kLength] = c; //stores selected character from key file in key[] array
-        kLength++;
-    }*/
-    
-
+    //double y[4]; //holds dy/dt values
+    int *y; //pointer for dy/dt values
+    y = (int *)malloc(numCores*sizeof(double)); //y points to starting address of array of size numCores, values are doubles
+    double cap[numCores]; //holds thermal capacitances
+    double pow[numCores]; //holds power values for specific time interval
+	double res[numCores][numCores]; //holds thermal resistances
+	double temp[numCores]; //holds starting temperatures for all cores
+	for(int ti = 0; ti < numCores; ti++){ //fills array
+		temp[ti] = ambient;
+	}
+	
     FILE *powerFile = fopen(argv[2], "r"); //Test File Input
     assert(powerFile != NULL); //Check to see file exists
 
+    //reading from power file and populating array
     
+
+
+    //let the spicy begin, call RK and obtain results
+    for(int steps = 0; steps < 200; steps++){  //200 iterations; 200h = 1 run through [0-Tau] and subsequent intervals
+    	rk(numCores){
+
+    	}
+    }
+
+
+
+
+
+
+
+
     FILE *outputFile = fopen(argv[3], "w"); //Test File Output
     assert(powerFile != NULL); //check to see file exists
 
-	while ((c = getc(powerFile)) != EOF){
-		temp=generateKeyByte(S)^c; // EX-OR key stream byte with input file byte
-		fputc(temp, outputFile); // write encrypted byte to specified output file
-    }
+	//writing to file stufferoni
 
 
 	fclose(paramFile); //Closes parameter file
