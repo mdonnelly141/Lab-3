e8a07b5cf41a0f4ab956fc694dd0fcca4ba082db
mdonnelly141 <matthewdonne@umass.edu>
2016-11-12 19:09:48 -0500

    Update main.c

--- compSysProject3/main.c
+++ compSysProject3/main.c
@@ -12,19 +12,21 @@
 #define h = 0.005				//step size for RK Algorithm
 const char EOL = '\n';			//end of line character
 double ambient = 300;			//default ambient temperature of 300k
+double startT = 300;			//starting temperature of cores @ 300k
 
 
 //////////////////////Runge-Kutta Algorithm//////////////////////////////////
-/*double *rk(int *numCores, double c[], double r[][], double p[]){   //returns pointer	
-	/*for(int i=0; i<*numCores; i++){ //handles interations for K(i,j)
+/*double *rk(int *numCores, double *c[], double *r[][], double *p[]){   //returns pointer	
+	int i;
+	for(i=0; i<*numCores; i++){ //handles interations for K(i,j)
 		if(row = col)  //checks for core comparison to itself
 			;		   //don't calculate if core is compared to itself	
-		else
+		else{
 			k1 = h*f(*numCores, res[row][col], cap[i], pow[i]);  
 			i++;
 		}
-	}	*/
-//}
+	}
+}	*/
 /////////////////////////////////////////////////////////////////////////////
 
 //////////////////////Functions utilized within RK/////////////////////////// 
@@ -40,7 +42,8 @@ double ambient = 300;			//default ambient temperature of 300k
 //double sum(int currCore){
 //	double ans;
 //	int curr = currCore;
-//	for(int si = 0; si < core; si++){
+//	int si;
+//	for(si = 0; si < core; si++){
 //		ans = temp[si] - ambient;
 //	}
 //	return ans;
@@ -54,7 +57,7 @@ int main(int argc, const char * argv[]) {
 	char c;				//holds characters for file reading
 	int numCores = 0;	//number of cores
 
-	//////////////initial read to find number of cores
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%initial read to find number of cores
 	FILE *iparamFile = fopen(argv[1], "r"); // reads parameter File
     assert(iparamFile != NULL); //check that file exists
     while((c = getc(iparamFile)) != EOL){ //checks that there are characters and that end of line hasn't been reached
@@ -64,54 +67,107 @@ int main(int argc, const char * argv[]) {
     numCores+=1; //count last core, holds total number of cores*/   
     printf("Number of cores = %d\n", numCores);
     fclose(iparamFile); //Closes parameter file
-    //////////////end initial read 
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%end initial read 
 
     FILE *paramFile = fopen(argv[1], "r"); // reads parameter File
     assert(paramFile != NULL); //check that file exists
+    
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%THERMAL CAPACITANCES
+    double cap[numCores]; //holds C values
+    int ca;
+    //fill capacitance array
+    for(ca= 0; ca<(numCores); ca++)
+    	fscanf(paramFile, "%lf", &cap[ca]);
+    /*int pr;
+    for(pr =0;pr<numCores;pr++){
+    	printf("values in array %lf\n", cap[pr]);
+    } //DEBUGGGGG*/
+
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%THERMAL RESISTANCES
+    double res[numCores+1][numCores+1];	//Holds R values
+	int cr;
+	int crr;	//fill resistance array
+	for(cr = 0; cr<numCores+1; cr++){ //row designates a core
+		for(crr = 0; crr<numCores+1; crr++) //column designates relationship to another core
+			fscanf(paramFile, "%lf", &res[cr][crr]);
+	}
+	// DEBUG
+	/*int ack;
+	for(ack = 0; ack <numCores+1;ack++){
+		printf("%lf ", res[0][ack]);
+	}
+	printf("\n");
+	for(ack = 0; ack <numCores+1;ack++){
+		printf("%lf ", res[1][ack]);
+	}
+	printf("\n");
+	for(ack = 0; ack <numCores+1;ack++){
+		printf("%lf ", res[2][ack]);
+	}
+	printf("\n");
+	for(ack = 0; ack <numCores+1;ack++){
+		printf("%lf ", res[3][ack]);
+	}
+	printf("\n");
+	for(ack = 0; ack <numCores+1;ack++){
+		printf("%lf ", res[4][ack]);
+	}
+	printf("\n"); //%%%%%%%%%%%%%%%%%%%*/
+	
+	/*int pr;
+	int prr;
+    for(pr =0;pr<numCores;pr++){
+    	for(prr = 0; pr<numCores; prr++)
+    		printf("values in array %lf\n", cap[pr]);
+    } //DEBUGGGGG*/
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% STARTING CORE TEMPS 
+	double temp[numCores];	//holds temp values
+	int ti;
+	for(ti = 0; ti < numCores; ti++)	//fills array
+		temp[ti] = startT;
+	/*for(ti = 0; ti<numCores;ti++)
+		printf("Core temp is %lf\n", temp[ti]); //DEBUGGG*/ 
+
+	fclose(paramFile);
+
+
+	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% POWER VALUES
+	FILE *powFile = fopen(argv[2], "r"); // reads power file
+    assert(powFile != NULL); //check that file exists
+
+	double pow[numCores]; 	
+	int pi;
+	for(pi = 0; pi<numCores; pi++)
+		fscanf(powFile, "%lf", &pow[pi]);
+	/*for(pi = 0; pi<numCores;pi++)
+		printf("UNLIMITED POWER is %lf\n", pow[pi]);*/
+
+
+
     //double y[4]; //holds dy/dt values
     int *y; //pointer for dy/dt values
     y = (int *)malloc(numCores*sizeof(double)); //y points to starting address of array of size numCores, values are doubles
-    double cap[numCores]; 						//holds thermal capacitances
-    int ca;
-    for(ca= 0; ca<numCores; ca++)
-    	fscanf(paramFile, "%lf", &cap[ca]); //count core
-    int pr;
-    for(pr =0;pr<numCores;pr++){
-    	printf("values in array %lf\n", cap[pr]);
-    }
-    double pow[numCores]; 						//holds power values for specific time interval
-	double res[numCores][numCores];				//holds thermal resistances
-	double temp[numCores];						//holds starting temperatures for all cores 
-	/*for(int ti = 0; ti < numCores; ti++)		//fills array
-		temp[ti] = ambient;
-	 
-	
-    FILE *powerFile = fopen(argv[2], "r"); //Test File Input
-    assert(powerFile != NULL); //Check to see file exists
-
-    //reading from power file and populating array
-    
+        
 
 
     //let the spicy begin, call RK and obtain results
-    for(int steps = 0; steps < 200; steps++){  //200 iterations; 200*h = 1 run through [0-Tau], (Tau - 2Tau], etc
-    	rk(%numCores)
+    /*int steps;
+    for(steps = 0; steps < 200; steps++){  //200 iterations; 200*h = 1 run through [0-Tau], (Tau - 2Tau], etc
+    	*rk(%numCores, %cap[], %res[][], %pow[]);
+    }*/
 
     	
-    }
+    //*rk(int *numCores, double c[], double r[][], double p[]){  
 
 
 
-    FILE *outputFile = fopen(argv[3], "w"); //Test File Output
+
+
+    /*FILE *outputFile = fopen(argv[3], "w"); //Test File Output
     assert(powerFile != NULL); //check to see file exists
 
-	//writing to file stufferoni
+	//writing to file stufferoni*/
 
-
-	fclose(paramFile); //Closes parameter file
-    fclose(powerFile); //Closes power file
-	fclose(outputFile);//Closes output file*/
-
-    
     return 0;
 }//end main
