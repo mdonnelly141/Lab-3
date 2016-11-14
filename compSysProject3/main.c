e6f4075cc7d935854a1bcb70d069daeae97c3176
Michael Zeimbekakis <mzeimbekakis@1x-nat-vl931-172-30-185-110.wireless.umass.edu>
2016-11-14 17:42:15 -0500

    working rk, temp values work. Need to write age code.

--- compSysProject3/main.c
+++ compSysProject3/main.c
@@ -29,21 +29,10 @@ double sum(int run, int core, int numCores);
 //double sum(int total, int currCore, int casex);
 
 //////////////////////Runge-Kutta Algorithm//////////////////////////////////
-void rk(int numCores, double time, int iteration){   //returns value
+
+void rk(int numCores, double time){   //returns value
 	int kSub1; // the value of k's first subscript
-    int kSub2 = 0; // the valu of k's second subscript
-    
-//    for(run = 0; run < numCores; run++)
-//		karr[0][run] = h*f(numCores, run, test);
-//    currCore = 1;
-//	for(run = 0; run < numCores; run++)
-//		karr[1][run] = h*f(numCores, run, test);
-//    currCore = 2;
-//	for(run = 0; run < numCores; run++)
-//		karr[2][run] = h*f(numCores, run, test);
-//    currCore = 3;
-//	for(run = 0; run < numCores; run++)
-//		karr[3][run] = h*f(numCores, run, test);
+    int kSub2 = 0; // the value of k's second subscript
     
     for (kSub1 = 0; kSub1<4; kSub1++) {
         for (kSub2 = 0; kSub2<numCores; kSub2++) {
@@ -52,6 +41,7 @@ void rk(int numCores, double time, int iteration){   //returns value
         }
         printf("\n");
     }
+    printf("\n");
     int run;
     for(run = 0; run<numCores; run++){
         temp[1][run] = temp[0][run] + (karr[0][run]+2*karr[1][run]+2*karr[2][run]+karr[3][run])/6.0;
@@ -59,59 +49,32 @@ void rk(int numCores, double time, int iteration){   //returns value
         printf("%lf\n",temp[1][run]);
     }
 }
-/////////////////////////////////////////////////////////////////////////////
 
-//////////////////////Functions utilized within RK/////////////////////////// 
-//double f(int numcore, int core, int casey){
-//	double k = 0;
-//	k = sum(numcore, core, casey);
-//	k = (power[core] - k)/cap[core];
-//	return k;
-//}
+//////////////////////Functions utilized within RK///////////////////////////
 
-double f(int kSub1, int kSub2, int numCores){
-    double k = power[kSub2]/cap[kSub2]-sum(kSub1,kSub2,numCores);
+double f(int kSub1, int kSub2, int numCores){ //pass current K, current core, number of cores
+    double k = (power[kSub1]/cap[kSub1])-sum(kSub1,kSub2,numCores);
     return k;
 }
 
 double sum(int kSub1, int kSub2, int numCores){
     int currNode;
     double ans = 0;
-    for(currNode = 0;currNode<numCores;currNode++){
+    for(currNode = 0;currNode<numCores+1;currNode++){
         if (kSub2!=currNode&&kSub1==0){
-            ans += (temp[0][kSub1]-temp[0][currNode])/(res[kSub1][currNode]*cap[kSub1]);
+            ans += (temp[1][kSub1]-temp[1][currNode])/(res[kSub1][currNode]*cap[kSub1]);
         }
         else if(kSub2!=currNode&&kSub1==3){
-            ans += (((temp[0][kSub1]+karr[kSub1-1][currNode])-(temp[0][currNode]+karr[kSub1-1][currNode]))/(res[kSub1][currNode]*cap[kSub1]));
+            ans += (((temp[1][kSub1]+karr[kSub1-1][currNode])-(temp[1][currNode]+karr[kSub1-1][currNode]))/(res[kSub1][currNode]*cap[kSub1]));
         }
         else if(kSub2!=currNode){
-            ans += (((temp[0][kSub1]+karr[kSub1-1][currNode]/2)-(temp[0][currNode]+karr[kSub1-1][currNode]/2))/(res[kSub1][currNode]*cap[kSub1]));
+            ans += (((temp[1][kSub1]+karr[kSub1-1][currNode]/2)-(temp[1][currNode]+karr[kSub1-1][currNode]/2))/(res[kSub1][currNode]*cap[kSub1]));
         }
     }
+//    printf("%lf\n",ans);
     return ans;
 }
 
-//double sum(int total, int currCore, int casex){
-//	int cou = 0;
-//	double ans = 0;
-//	while(cou<total){
-//        if(currCore != cou){
-//            if(casex == 0) //calculate sum for K1
-//                ans += (temp[currCore] - temp[cou])/res[currCore][cou];
-//            else if(casex == 1) //calculate sum for k2
-//                ans += (((temp[currCore]+(karr[0][currCore])/2)-(temp[cou]+(karr[0][cou])/2))/res[currCore][cou]);
-//            else if(casex == 2) //calculate sum for k3
-//                ans += (((temp[currCore]+(karr[1][currCore])/2)-(temp[cou]+(karr[1][cou])/2))/res[currCore][cou]);
-//            else if(casex == 3) //calculate sum for k4
-//                ans += ((temp[currCore]+karr[2][currCore])-(temp[cou]+karr[2][cou]))/res[currCore][cou];
-//        }
-//        else
-//            ;
-//        cou++;
-//        printf("%lf\n",ans);
-//	}
-//	return ans;
-//}
 ////////////////////////////////////////////////////////////////////////////
 
 int powLine(FILE *powFile,int numCores){
@@ -135,6 +98,7 @@ int main(int argc, const char * argv[]) {
 	
 
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%initial read to find number of cores
+    
 	FILE *iparamFile = fopen(argv[1], "r"); // reads parameter File
     assert(iparamFile != NULL); //check that file exists
     while((c = getc(iparamFile)) != EOL){ //checks that there are characters and that end of line hasn't been reached
@@ -145,7 +109,8 @@ int main(int argc, const char * argv[]) {
     printf("Number of cores = %d\n", numCores);
     fclose(iparamFile); //Closes parameter file
 
-    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%end initial read 
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%end initial read
+    
     FILE *paramFile = fopen(argv[1], "r"); // reads parameter File
     assert(paramFile != NULL); //check that file exists
 
@@ -155,17 +120,8 @@ int main(int argc, const char * argv[]) {
     for(kc = 0; kc<numCores; kc++)
     	karr[kc] = (double*)calloc(numCores, sizeof(double));
 
-        
+    
     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%THERMAL CAPACITANCES
-    /*double cap[numCores]; //holds C values
-    int ca;
-    //fill capacitance array
-    for(ca= 0; ca<(numCores); ca++)
-    	fscanf(paramFile, "%lf", &cap[ca]);
-    int pr;
-    for(pr =0;pr<numCores;pr++){
-    	printf("values in array %lf\n", cap[pr]);
-    } //DEBUGGGGG*/
     
     cap = (double *)malloc(numCores*sizeof(double));
     int catch = 0;
@@ -179,14 +135,7 @@ int main(int argc, const char * argv[]) {
     
     
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%THERMAL RESISTANCES
-    /*double res[numCores+1][numCores+1];	//Holds R values
-	int cr;
-	int crr;	//fill resistance array
-	for(cr = 0; cr<numCores+1; cr++){ //row designates a core
-		for(crr = 0; crr<numCores+1; crr++) //column designates relationship to another core
-			fscanf(paramFile, "%lf", &res[cr][crr]);
-	}*/
-	
+    
     res = (double**)malloc((numCores+1)*sizeof(double*));
     int i = 0;
     for(i=0; i<numCores+1; i++){
@@ -199,43 +148,10 @@ int main(int argc, const char * argv[]) {
             fscanf(paramFile, "%lf", &(res[cr][crr]));
         
     }
-	// DEBUG
-	/*int ack;
-	for(ack = 0; ack <numCores+1;ack++){
-		printf("%lf ", res[0][ack]);
-	}
-	printf("\n");
-	for(ack = 0; ack <numCores+1;ack++){
-		printf("%lf ", res[1][ack]);
-	}
-	printf("\n");
-	for(ack = 0; ack <numCores+1;ack++){
-		printf("%lf ", res[2][ack]);
-	}
-	printf("\n");
-	for(ack = 0; ack <numCores+1;ack++){
-		printf("%lf ", res[3][ack]);
-	}
-	printf("\n");
-	for(ack = 0; ack <numCores+1;ack++){
-		printf("%lf ", res[4][ack]);
-	}
-	printf("\n"); //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
-	
-	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% STARTING CORE TEMPS 
-	/*double temp[numCores];	//holds temp values
-	int ti;
-	for(ti = 0; ti < numCores; ti++)	//fills array
-		temp[ti] = startT;*/
-	/*for(ti = 0; ti<numCores;ti++)
-		printf("Core temp is %lf\n", temp[ti]); //DEBUGGG*/ 
-	
-//	temp = (double *)malloc(numCores*sizeof(double));
-
 	fclose(paramFile);
 
-
 	//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% POWER VALUES
+    
 	FILE *powFile = fopen(argv[2], "r"); // reads power file
     assert(powFile != NULL); //check that file exists
 
@@ -256,7 +172,6 @@ int main(int argc, const char * argv[]) {
         cont = powLine(powFile, numCores);
         lines++;
     }
-    //////////////////////////////rewind(powFile);
     fclose(powFile);
     FILE *powFile2 = fopen(argv[2], "r");
     assert(powFile2 != NULL);
@@ -285,15 +200,19 @@ int main(int argc, const char * argv[]) {
     int steps;
     double time = 0;
     int line;
-    rk(numCores,0,0);
+    rk(numCores,0);
     time+=h;
     for(line = 1;line<lines;line++){
         for(steps = 1; steps <= (1/h); steps++){  //200 iterations; 200*h = 1 run through [0-Tau], [Tau - 2Tau], etc
-            rk(numCores,time,steps);
+            rk(numCores,time); //returns temp values for current interval of time
+            
+            //write to output file hello
+            
             time += h;
         }
         powLine(powFile2, numCores);
     }
+    
     	
     //*rk(int *numCores, double c[], double r[][], double p[]){  
 
